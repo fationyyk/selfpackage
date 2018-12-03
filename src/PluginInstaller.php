2f86ba2f64195be4a8ab07bb9f8198f2ef231756
root <root@localhost.localdomain>
2018-12-03 23:19:35 +0800

    self-package

---
+++ src/PluginInstaller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace fationyyk\selfpackage;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class PluginInstaller extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        
+        $prefix = substr($package->getPrettyName(), 0, 23);
+        
+        if ('fationyyk/push' !== $prefix) {
+            throw new \InvalidArgumentException(
+                'Unable to install template, phpdocumentor templates '
+                .'should always start their package name with '
+                .'"phpdocumentor/template-"'
+            );
+        }
+        return './data/templates/'.substr($package->getPrettyName(), 23);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+       
+        return 'phpdocumentor-template' === $packageType;
+    }
+}
